ceb51dbcc40ac956ceaad3cc261febf7bc363407
Mateo Bustamante <mateogbustamante@gmail.com>
2025-10-24 16:32:52 -0300

    creación del superusuario y las funcionalidades list_display y search_fields

--- estudiante/admin.py
+++ estudiante/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Curso, Estudiante
 
 # Register your models here.
+
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'cantidad_horas')
+    search_fields = ('nombre',)
+
+@admin.register(Estudiante)
+class EstudianteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellido', 'edad', 'nota_curso', 'curso')
+    search_fields = ('nombre', 'apellido')
